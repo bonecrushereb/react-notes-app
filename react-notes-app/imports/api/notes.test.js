74e38f81c128519b6c19c15f050a38300daf8db3
bonecrushereb <bonecrushereb@gmail.com>
2017-09-28 13:22:12 -0700

    working tests pasts as well as error

--- react-notes-app/imports/api/notes.test.js
+++ react-notes-app/imports/api/notes.test.js
@@ -8,9 +8,14 @@ if (Meteor.isServer) {
     it('should insert a new note', function () {
       const userId = 'testid';
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
-      console.log(_id ,typeof(_id));
 
-      expect(Notes.findOne({ _id, userId })).should.exist();
+      expect(Notes.findOne({ _id, userId })).toBeTruthy();
+    });
+
+    it('should not insert a new note if not authenticated', function () {
+      expect(() => {
+        Meteor.server.method_handlers['notes.insert']();
+      }).toThrow();
     });
   });
 }
