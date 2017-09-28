8db2aeb525daea25c260dbaa4fce204b68855fd2
bonecrushereb <bonecrushereb@gmail.com>
2017-09-28 14:12:14 -0700

    all remove tests passing

--- react-notes-app/imports/api/notes.test.js
+++ react-notes-app/imports/api/notes.test.js
@@ -5,6 +5,18 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
+
+    beforeEach(function () {
+      Notes.remove({});
+      Notes.insert({
+        _id: 'testNoteId1',
+        title: 'test',
+        body: 'my body test',
+        updateAt: 0,
+        userId: 'testUserId1'
+      })
+    });
+
     it('should insert a new note', function () {
       const userId = 'testid';
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
@@ -15,6 +27,25 @@ if (Meteor.isServer) {
     it('should not insert a new note if not authenticated', function () {
       expect(() => {
         Meteor.server.method_handlers['notes.insert']();
+      }).toThrow();
+    });
+
+    it('should remove a note', function() {
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+
+      expect(Notes.findOne({ _id: 'testNoteId1' })).toBeFalsy();
+    });
+
+    it('should not remove a note if unauthenticated', function () {
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1']);
+      }).toThrow();
+    });
+
+    it('should not remove note if invalid _id', function () {
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' });
+
       }).toThrow();
     });
   });
