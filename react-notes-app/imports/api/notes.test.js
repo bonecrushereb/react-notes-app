d227e7d5215bf72a52401a4e599cff2a2c76347a
bonecrushereb <bonecrushereb@gmail.com>
2017-09-29 15:24:10 -0700

    update test failing

--- react-notes-app/imports/api/notes.test.js
+++ react-notes-app/imports/api/notes.test.js
@@ -5,16 +5,17 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
-
-    beforeEach(function () {
-      Notes.remove({});
-      Notes.insert({
+    const noteOne = {
         _id: 'testNoteId1',
         title: 'test',
         body: 'my body test',
         updateAt: 0,
         userId: 'testUserId1'
-      })
+    };
+
+    beforeEach(function () {
+      Notes.remove({});
+      Notes.insert(noteOne);
     });
 
     it('should insert a new note', function () {
@@ -31,22 +32,40 @@ if (Meteor.isServer) {
     });
 
     it('should remove a note', function() {
-      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
 
-      expect(Notes.findOne({ _id: 'testNoteId1' })).toBeFalsy();
+      expect(Notes.findOne({ _id: noteOne._id })).toBeFalsy();
     });
 
     it('should not remove a note if unauthenticated', function () {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1']);
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id]);
       }).toThrow();
     });
 
     it('should not remove note if invalid _id', function () {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' });
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId });
 
       }).toThrow();
+    });
+
+    it('should update a note', function () {
+      const title = 'updatedTitle';
+      Meteor.server.method_handlers['notes.update'].apply({
+        userId: noteOne.userId
+      }, [
+        noteOne._id,
+        { title }
+      ]);
+
+      const note = Notes.findOne(noteOne._id);
+
+      expect(note.updatedAt).toBeGreaterThan(0);
+      expect(note).toContain({
+          title,
+          body: noteOne.body
+        });
     });
   });
 }
