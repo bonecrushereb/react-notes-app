cc8928b8c59d0f514cf67eb76e3ea2a82a034937
bonecrushereb <bonecrushereb@gmail.com>
2017-10-02 14:16:03 -0700

    testing for logout done

--- react-notes-app/imports/ui/PrivateHeader.js
+++ react-notes-app/imports/ui/PrivateHeader.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import  { createContainer } from 'meteor/react-meteor-data';
 
-const PrivateHeader = (props) => {
+export const PrivateHeader = (props) => {
   return (
     <div className="header">
       <div className="header__content">
         <h1 className="header__title">{props.title}</h1>
-      <button className="button button--link-text" onClick={props.handleLogout()}>Logout</button>
+        <button className="button button--link-text" onClick={props.handleLogout()}><Link to="/">Logout</Link></button>
       </div>
     </div>
   );
@@ -18,4 +20,8 @@ PrivateHeader.propTypes = {
   handleLogout: propTypes.func.isRequired
 };
 
-export default PrivateHeader;
+export default createContainer(() => {
+  return {
+    handleLogout: () => Accounts.logout();
+  };
+}, PrivateHeader);
