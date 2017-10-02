94558d2a42071e822f737f74aa4e9418bb79003e
bonecrushereb <bonecrushereb@gmail.com>
2017-10-02 12:40:50 -0700

    first initial spy

--- react-notes-app/imports/ui/PrivateHeader.test.js
+++ react-notes-app/imports/ui/PrivateHeader.test.js
@@ -2,14 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
-import { withRouter } from 'react-router-dom';
 
 import PrivateHeader from './PrivateHeader';
 
 if(Meteor.isClient) {
   describe('PrivateHeader', function() {
     it('should set button text to logout', function() {
-      const wrapper = mount( <PrivateHeader title="Test title"/> );
+      const wrapper = mount( <PrivateHeader title="Test title" handleLogout={() => {}}/> );
       const buttonText = wrapper.find('button').text();
 
       expect(buttonText).toBe('Logout');
@@ -17,10 +16,19 @@ if(Meteor.isClient) {
 
     it('should use title prop as h1 text', function() {
       const title = 'Title test here';
-      const wrapper = mount( <PrivateHeader title={title}/> );
+      const wrapper = mount( <PrivateHeader title={title} handleLogout={() => {}}/> );
       const h1Text = wrapper.find('h1').text();
 
       expect(h1Text).toBe('Title test here')
-    })
+    });
+
+    it('should call handleLogout on click', function() {
+      const spy = expect.createSpy();
+      const wrapper = mount( <PrivateHeader title="title" handleLogout={spy}/> );
+
+      wrapper.find('button').simulate('click');
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 }
