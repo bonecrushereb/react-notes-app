54576b3b2b1af34afdc2c86c6348ea7b71e7d929
bonecrushereb <bonecrushereb@gmail.com>
2017-10-04 13:26:26 -0700

    work on loginwithpassword failing

--- react-notes-app/imports/ui/Login.test.js
+++ react-notes-app/imports/ui/Login.test.js
@@ -24,5 +24,41 @@ if(Meteor.isClient) {
       login.setState({ error: '' });
       expect(wrapper.find('p').length).toBe(0);
     });
+
+    it('should call loginWithPassword with the form data', function() {
+      const email = 'ben@test.com';
+      const password =  'password123';
+      const spy = expect.createSpy();
+      const wrapper = mount(
+        <MemoryRouter intialEntries={['/']} initalIndex={0}>
+          <Login loginWithPassword={spy}/>
+        </MemoryRouter>
+      );
+
+      const login = wrapper.find(Login).node;
+
+      login.refs['email'].value = email;
+      login.refs['password'].value = password;
+
+      wrapper.find('form').simulate('submit');
+
+      expect(spy.calls[0].arguments[0]).toEqual({ email });
+      expect(spy.calls[0].arguments[1]).toBe(password);
+    });
+
+    it('should set loginWithPassword callback errors', function() {
+      const spy = expect.createSpy();
+      const wrapper = mount(
+        <MemoryRouter intialEntries={['/']} initalIndex={0}>
+          <Login loginWithPassword={spy}/>
+        </MemoryRouter>
+      );
+
+      console.log(wrapper.find('form.boxed-view__form').simulate('submit'));
+
+      spy.calls[0].arguments[2]({});
+      expect(wrapper.state('error')).toNotBe(null);
+
+    });
   });
 }
--- react-notes-app/imports/ui/PrivateHeader.test.js
+++ react-notes-app/imports/ui/PrivateHeader.test.js
@@ -7,6 +7,7 @@ import { PrivateHeader } from './PrivateHeader';
 
 if(Meteor.isClient) {
   describe('PrivateHeader', function() {
+
     it('should set button text to logout', function() {
       const wrapper = mount( <PrivateHeader title="Test title" handleLogout={() => {}}/> );
       const buttonText = wrapper.find('button').text();
