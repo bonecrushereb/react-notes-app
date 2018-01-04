e7587c95e1e4c2379630e228af918b87b51d896f
bonecrushereb <bonecrushereb@gmail.com>
2018-01-04 12:46:54 -0800

    testing NoteListItem

---
+++ react-notes-app/imports/ui/NoteListItem.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import expect from 'expect';
+import { Meteor } from 'meteor/meteor';
+import { mount } from 'enzyme';
+
+import NoteListItem from './NoteListItem';
+
+if(Meteor.isClient) {
+  describe('NoteListItem', function() {
+
+    it('should render title and timestamp', function() {
+      const title = 'my title';
+      const updatedAt = 1515096500046;
+      const wrapper = mount( <NoteListItem note={{title, updatedAt}}/> )
+
+      expect(wrapper.find('h5').text()).toBe(title);
+      expect(wrapper.find('p').text()).toBe('1/04/18');
+    });
+  }); //end of describe
+}
