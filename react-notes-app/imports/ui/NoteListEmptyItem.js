3bca1e7e1e07055e8484dde39b4d1c1066a80bdb
bonecrushereb <bonecrushereb@gmail.com>
2018-01-10 12:20:20 -0800

    add to NoteList with ternary

---
+++ react-notes-app/imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+
+export default () => {
+  return (
+    <div>
+      <h5>You have no notes</h5>
+      <p>Create a note to get started!</p>
+    </div>
+  );
+};
