fbb2ecd48cada2e3087aae2bd8377321f59afdc9
bonecrushereb <bonecrushereb@gmail.com>
2018-01-14 22:36:57 -0800

    add fixtures for multiple files with similar data, added test to NoteListItem

---
+++ react-notes-app/imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const  notes = [
+    {
+      _id: 'noteId1',
+      title: 'Test title',
+      body: '',
+      updatedAt: 1515996693013,
+      userId: 'userId1'
+    }, {
+      _id: 'noteId2',
+      title: '',
+      body: 'something is here',
+      updatedAt: 1515996693013,
+      userId: 'userId2'
+    }
+];
