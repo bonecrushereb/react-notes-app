e2a6551131927a8dc895325b21b8c55ee9008bce
bonecrushereb <bonecrushereb@gmail.com>
2018-04-03 13:18:55 -0700

    editor tests started

--- react-notes-app/imports/ui/Editor.test.js
+++ react-notes-app/imports/ui/Editor.test.js
@@ -23,5 +23,24 @@ if (Meteor.isClient) {
       const wrapper = mount(<Editor history={history} call={call}/>);
       expect(wrapper.find('p').text()).toBe('Pick or create a note to get started.');
     });
-  });
+
+    it('should render not found message', function() {
+      const wrapper = mount(<Editor history={history} call={call} selectedNoteId={notes[0]._id}/>);
+      expect(wrapper.find('p').text()).toBe('Note not found.');
+    });
+
+    it('should remove note', function() {
+      const wrapper = mount(<Editor history={history} call={call} selectedNoteId={notes[0]._id} note={notes[0]}/>);
+
+      wrapper.find('button').simulate('click');
+
+      expect(history.push).toHaveBeenCalledWith('/dashboard');
+      expect(call).toHaveBeenCalledWith('notes.remove', notes[0]._id);
+    });
+
+    it('should update the note body on textarea change', function() {
+      const wrapper = mount(<Editor history={history} call={call} selectedNoteId={notes[0]._id} note={notes[0]}/>);
+
+    });
+  }); //end of describe
 }
