cd4e47293aaa361bb0504c76a582bd7d3f3dc31f
bonecrushereb <bonecrushereb@gmail.com>
2018-04-03 23:53:09 -0700

    add Session and test Session to NoteList

--- react-notes-app/imports/ui/NoteListHeader.js
+++ react-notes-app/imports/ui/NoteListHeader.js
@@ -2,23 +2,30 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import propTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session }  from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-          props.meteorCall('notes.insert');
+          props.meteorCall('notes.insert', (err, res) => {
+            if (res) {
+              props.Session.set('selectedNoteId', res);
+            }
+          });
         }}>Create Note</button>
     </div>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: propTypes.func.isRequired
+  meteorCall: propTypes.func.isRequired,
+  Session: propTypes.object.isRequired
 }
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader);
