366502bc919976fcdffecf5327eb9525e4259de7
bonecrushereb <bonecrushereb@gmail.com>
2018-04-05 14:07:51 -0700

    style notelist

--- react-notes-app/imports/ui/NoteListEmptyItem.js
+++ react-notes-app/imports/ui/NoteListEmptyItem.js
@@ -3,10 +3,7 @@ import { Meteor } from 'meteor/meteor';
 
 const NoteListEmptyItem = () => {
   return (
-    <div>
-      <h5>You have no notes</h5>
-      <p>Create a note to get started!</p>
-    </div>
+    <p className="empty-item">Create a note to get started!</p>
   );
 };
 
